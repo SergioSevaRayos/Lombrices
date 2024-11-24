9bd46c74f115e3d2fa553494fcbc48adbe9e1946
SergioSevaRayos <sergiosevarayos@gmail.com>
2024-11-24 08:28:12 +0100

    v0.1 versión mejorada

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
 // Lista inicial de productos
 const productos = [
-    { id: 1, nombre: "Cebo vivo", descripcion: "Cebos para surfcasting" },
-    { id: 2, nombre: "Señuelos", descripcion: "Señuelos para spinnig" },
-    { id: 3, nombre: "Aparejos", descripcion: "Componentes para aparejos" },
-    { id: 4, nombre: "Cebo para pulpos", descripcion: "Cebo dedicado a la pesca con nasas" },
-    { id: 5, nombre: "Contacto", descripcion: "Ponte en contacto" }
+    { id: 1, nombre: "Cebo vivo", descripcion: "Cebos para surfcasting", imagen: "media/img/bloodworm2.png" },
+    { id: 2, nombre: "Señuelos", descripcion: "Señuelos para spinnig", imagen: "media/img/señuelo.png" },
+    { id: 3, nombre: "Aparejos", descripcion: "Componentes para aparejos", imagen: "media/img/plomo.png" },
+    { id: 4, nombre: "Cebo para pulpos", descripcion: "Cebo dedicado a la pesca con nasas", imagen: "media/img/pulpo1.png" },
+    { id: 5, nombre: "Contacto", descripcion: "Ponte en contacto", imagen: "media/img/contacto.png" }
 ];
+
 
 // Generar el menú de productos
 function generarMenu() {
@@ -26,6 +27,7 @@ function generarProductos() {
         const div = document.createElement("div");
         div.classList.add("producto");
         div.innerHTML = `
+            <img src="${producto.imagen}" alt="${producto.nombre}" />
             <h3>${producto.nombre}</h3>
             <p>${producto.descripcion}</p>
         `;
